bfb186a17b3f5af5ef4640ea4fde44efbf5df916
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-08-22 15:53:29 +0300

    done 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+  .map(el => {
+    return `
+     <a class="gallery__link" href="${el.original}">
+    <img
+          class="gallery__image"
+          src="${el.preview}"
+          data-source="${el.original}"
+          alt="${el.description}"
+        />
+</a>`;
+  })
+  .join('');
+
+galleryBox.insertAdjacentHTML('afterbegin', galleryEl);
+
+// * SimpleLightbox
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+import throttle from 'lodash.throttle';
 
+const onPlay = function () {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+};
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    console.log(e.seconds);
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+player.on('play', onPlay);
