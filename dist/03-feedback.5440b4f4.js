6f151c4e537af62589ed1cdf1f684ea3975665ae
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-08-23 15:21:47 +0300

    Finished all tasks

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -507,34 +507,36 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _lodashThrottle = require("lodash.throttle");
 var _lodashThrottleDefault = parcelHelpers.interopDefault(_lodashThrottle);
 const form = document.querySelector(".feedback-form");
-const emailInput = document.querySelector("input");
-const messageInput = document.querySelector("textarea");
 const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+//
+autofill();
+function autofill() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (!savedData) return;
+    Object.entries(savedData).forEach(([name, value])=>{
+        form.elements[name].value = value;
+    });
+}
 function onFormSubmit(e) {
     e.preventDefault();
+    const { email , message  } = e.target;
+    if (email.value === "" || message.value === "") return alert("Please fill in all the fields!");
+    const obj = {
+        email: form.email.value,
+        message: form.message.value
+    };
+    console.log(obj);
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
-function onEmailInput(e) {
-    localStorage.setItem(STORAGE_KEY, e.target.value);
-    console.log(e.target.value);
-}
 function onFormInput(e) {
-    console.log(e.target.value);
+    const { name , value  } = e.target;
+    formData[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 form.addEventListener("submit", onFormSubmit);
-emailInput.addEventListener("input", (0, _lodashThrottleDefault.default)(onEmailInput, 500));
-form.addEventListener("input", onFormInput);
-function autofill() {
-    const savedData = localStorage.getItem(STORAGE_KEY);
-    console.log(savedData);
-    if (savedData) {
-        console.log("yes");
-        emailInput.value = savedData;
-    // messageInput.value = savedData;
-    }
-}
-autofill();
+form.addEventListener("input", (0, _lodashThrottleDefault.default)(onFormInput, 500));
 
 },{"lodash.throttle":"bGJVT","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"bGJVT":[function(require,module,exports) {
 var global = arguments[3];
